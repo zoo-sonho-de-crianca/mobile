38e51c42ba17b7f046f044c65cc7e23d38570240
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-24 20:18:45 -0300

    feat(auth): add auth screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,31 @@
 import { Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import {
+  Urbanist_700Bold,
+  Urbanist_400Regular,
+  useFonts,
+} from "@expo-google-fonts/urbanist";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [loaded, error] = useFonts({
+    Urbanist_700Bold,
+    Urbanist_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return <GluestackUIProvider mode="light"><Stack screenOptions={{ headerShown: false }} /></GluestackUIProvider>;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,70 @@
 import { View, Text, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import LinkButton from "./components/LinkButton";
+import { Link } from "expo-router";
+import { urbanist } from "../styles/typography";
 
-export default function SplashScreen() {
+const appImage = require("../../assets/images/app_logo.png");
+
+export default function AuthScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>Meu App</Text>
+      <Image source={appImage} style={styles.image} />
+
+      <View style={styles.greeting}>
+        <Text style={[urbanist.bold, { fontSize: 32 }]}>Bem-vindo (a)!</Text>
+        <Text style={[urbanist.regular, { fontSize: 18 }]}>
+          Faça login ou cadastre-se
+        </Text>
+      </View>
+
+      <View style={styles.buttonsView}>
+        <LinkButton label="Entrar" link="./screens/Auth/SignInScreen" />
+        <LinkButton
+          label="Cadastre-se"
+          link="./screens/Auth/SignUpScreen"
+          variant="lightOrange"
+        />
+      </View>
+
+      <View style={styles.policies}>
+        <Link href="/screens/Policy/PrivacyPolicy">
+          <Text style={urbanist.regular}>Política de Privacidade</Text>
+        </Link>
+        <Text style={styles.circle}>·</Text>
+        <Link href="/screens/Policy/ServiceTerms">
+          <Text style={urbanist.regular}>Termos de Serviço</Text>
+        </Link>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  logo: { fontSize: 32, fontWeight: "bold" },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 36,
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 80,
+    height: 80,
+  },
+  greeting: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonsView: {
+    width: "100%",
+    gap: 20,
+  },
+  policies: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  circle: {
+    paddingHorizontal: 12,
+  },
 });
