a0810c00a315ed499e040cfffdc57b82100dd9aa
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-24 15:12:12 -0300

    setup(app): change initial template app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { useRouter } from "expo-router";
-import { hasSeenOnboarding } from "../utils/storage";
 
 export default function SplashScreen() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const check = async () => {
-      const seen = await hasSeenOnboarding();
-      router.replace(seen ? "/home" : "/onboarding");
-    };
-    setTimeout(check, 1500);
-  }, []);
-
   return (
     <View style={styles.container}>
       <Text style={styles.logo}>Meu App</Text>
